246d33d87ebe33a337f5e4f2b800e8fb113a2f33
Mungage <Mungage@gmail.com>
2020-12-25 17:39:26 +0100

    Updated the filip solution

--- filip/filip.py
+++ filip/filip.py
@@ -1,14 +1,37 @@
 import sys
 
-#Take two ints from stdin
+#Take input from stdin
 for string in sys.stdin:
-    answers = string.split()
-    #reverse the two numbers
-    a = int(str(answers[0][::-1]))
-    b = int(str(answers[1][::-1]))    
 
-#compare and print which one is the largest
-print(max(a, b))
+    #Check if input string contains any zeroes
+    if "0" in string:
+        print("Sorry, the inputs contained a zero")
+    else:
+        #Turn input into two separate string variables
+        answers = string.split()
+        #Reverse the two foremost strings and turn them into ints
+        a = int(str(answers[0][::-1]))
+        b = int(str(answers[1][::-1]))
+        #Check to see if the two numbers are equal to one another
+        if a != b:
+            #Compare the two ints and print the larger one
+            print(max(a, b))
+        else:
+            print("Sorry, the inputs cannot be equal")
+
+
+        
+        
+    
+
+
+
+
+   
+   
+
+
+   
 
 
 
