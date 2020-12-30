bc850e22f698d6e6e56413c50855d0cdaea30875
Mungage <Mungage@gmail.com>
2020-12-30 19:50:33 +0100

    Almost done with problem_a

--- problem_a/problem_a2.py
+++ problem_a/problem_a2.py
@@ -35,15 +35,16 @@ def main():
     # Format the list of inputs into a mixed list of strings and ints
     formatted_list = [format_list(n) for n in format_list(inputs)]
     
-    # Set the T variable to the first index
+    # Set the T variable to the first index, and n to all ints after the first index
     T = formatted_list[0]
+    n = []
+    get_number_of_postcards(formatted_list, n)
     
     # Get a sliced list containing the postcard/cities 
-
-    list_of_postcards = slice_list(formatted_list)
+    list_of_postcards = slice_list(formatted_list, n)
 
     # Check all the given constraints and assign some variables
-   # print(check_constraints(formatted_list, T, list_of_postcards))
+    print(check_constraints(formatted_list, T, n, list_of_postcards))
 
     # Print the result, which should be the number of unique postcards for each year 
     #check_unique_cities(list_of_postcards, T)
@@ -54,31 +55,40 @@ def format_list(inputs):
     except (ValueError, TypeError):
         return inputs
 
-def check_constraints(formatted_list, T, list_of_postcards):
-    n = 0
-
-    print(list_of_postcards)
-    # Get the number n for each actual year in the list of inputs
-    for postcard in formatted_list[1:]:
-        if isinstance(postcard, int):
-
-            if postcard > 0 and postcard <= 100:
-                n += 1
-                print(n, postcard)
-            else:
-                return "One of the given years broke a constraint"
-    
-        else:
+def set_number_of_postcards(formatted_list, n):
+    for item in formatted_list[1:]:
+        try:
+            n.append(int(item))
+        except ValueError:
             pass
 
+def check_constraints(formatted_list, T, n, list_of_postcards):
+    
+    #print(formatted_list)
+    #print(T)
+    #print(n)
+    #print(list_of_postcards)
+
+    if not T == len(n) and T > 0 and T <= 50:
+        return False
+
+    i = 0
+    for n in n:
+        if not n == len(list_of_postcards[i]):
+            return False
+        else:
+            i += 1
+
+    return True
+        
+
+    
+
+
+        
     # Check whether the number of cities after each n is equal to the n
 
     # Check whether the T is equal to the number of ints n after range [1:]
-    if T == n and T > 0 and T <= 50:
-        pass
-    else:
-        return "The number of years to investigate is not equal to the number of actual years provided"
-    return(True)
     
     # Given constraints:
     # T <= 50   Done
@@ -88,6 +98,14 @@ def check_constraints(formatted_list, T, list_of_postcards):
     # the number T must be equal to the number of years     Done
     # the number n of each year must be equal to the number of strings
 
+def get_number_of_postcards(formatted_list, n):
+    for item in formatted_list[1:]:
+        try:
+            n.append(int(item))
+        except ValueError:
+            pass
+
+"""
 def get_indexes(temp_list):
     indexes = []
 
@@ -99,22 +117,15 @@ def get_indexes(temp_list):
             pass
 
     return indexes
-
-def slice_list(formatted_list):
+"""
+def slice_list(formatted_list, n):
     temp_list = formatted_list.copy()
     sliced_list = [] 
     temp_list.pop(0)   # We remove the first index from the list we get
-    indexes = get_indexes(temp_list)
 
-    print(indexes)     # 0, 8
-
-    #print(len(temp_list))
-
-    #print(temp_list)
-    i = 0
     j = 0
     
-    for i in range(len(indexes)):
+    for i in range(len(n)):
         new_list = []
         sliced_list.append(new_list)
 
@@ -125,33 +136,8 @@ def slice_list(formatted_list):
         else:
             j += 1
 
-    print(sliced_list)
-        
-    """
-    for index, item in enumerate(temp_list):
-
-        if isinstance(item, int):
-            start = temp_list.index(item) + 1
-            stop = item + start
-            sliced_list.append(temp_list[start:stop])
-   # print(temp_list)
-   # print(sliced_list)
-
-# om inte det inputtade nummret är samma som mängden 
-
-    for index, item in enumerate(temp_list):
-        
-        if isinstance(item, int):
-            startslice = temp_list.index(item) + 1
-            stopslice = item + startslice
-            #print(startslice, stopslice, "\n-------")
-            sliced_list.append(temp_list[startslice: stopslice])
-            print(sliced_list, "\n-------")
-
-    # Return a sliced list containing as many lists as needed
-    return sliced_list
-    """
-
+    return(sliced_list)
+ 
 def check_unique_cities(sliced_list, T):
     for num in range(T):
         print(len(set(sliced_list[num])))
